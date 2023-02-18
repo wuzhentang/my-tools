7742e95fe43cf6f27be35e642edaa74dac060391
Howell <wuzhentang@hotmail.com>
2023-02-18 23:07:14 +0800

    feat(bin): add dex2jar tools

--- shell-extends/dev.sh
+++ shell-extends/dev.sh
@@ -2,5 +2,5 @@
 alias printEnvs="xargs -0 -L1 -a /proc/self/environ"
 alias mkcscopefile="find \`pwd\` -name \"*.[ch]\" -o -name \"*.cpp\" -o -name \"*.java\" > cscope.files"
 alias mkctags="ctags -R -L cscope.files && cscope -Rbqv -i cscope.files"
-alias dex2jar="~/bin/dex2jar-2.1/dex-tools-2.1/d2j-dex2jar.sh"
+alias dex2jar="$CURRENT_PATH/bin/common/dex-tools-2.1/d2j-dex2jar.sh"
 
